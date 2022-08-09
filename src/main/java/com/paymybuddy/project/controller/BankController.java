e65d8dfefb87bee2514a524a151e291de07e5883
libra1919 <aymentli@gmail.com>
2022-08-09 16:20:16 +0200

    suite controller readme

---
+++ src/main/java/com/paymybuddy/project/controller/BankController.java
@@ -0,0 +1,70 @@
+package com.paymybuddy.project.controller;
+
+import com.paymybuddy.project.dto.BankDTO;
+import com.paymybuddy.project.model.Bank;
+import com.paymybuddy.project.model.User;
+import com.paymybuddy.project.service.BankService;
+import com.paymybuddy.project.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public class BankController {
+
+    @Autowired
+    private BankService bankTransac;
+
+    @Autowired
+    private UserService userService;
+
+    Logger LOGGER = LoggerFactory.getLogger(BankController.class);
+
+    /**
+     * Process bank transaction
+     * @param bankDTO transaction infos
+     * @param userId userID
+     * @return Status 200 if transaction OK, status 400 if failed
+     */
+    @PostMapping(value = "bankTransaction/{userId}")
+    public ResponseEntity<BankDTO> processTransaction (@RequestBody BankDTO bankDTO, @PathVariable Long userId){
+        if(userService.getUserById(userId).isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        User user = userService.getUserById(userId).get();
+        Bank bank = bankTransac.createBankTransaction(user, bankDTO.getIban(),bankDTO.getAmount());
+        if(bankTransac.processTransaction(bank)){
+            bankTransac.save(bank);
+            LOGGER.info("Process Bank Transaction success");
+            return ResponseEntity.ok(bankDTO);
+        } else {
+            LOGGER.info("Invalid ID, transaction failed");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    /**
+     * List of bank transactions by user
+     * @param userId the userID
+     * @return list of bank transaction, 400 if the user doesn't exist
+     */
+    @GetMapping(value = "bankTransaction/{userId}")
+    public ResponseEntity<List<Bank>> getTransaction(@PathVariable Long userId){
+        if(userService.getUserById(userId).isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        User user = userService.getUserById(userId).get();
+        List<Bank> listTransac = bankTransac.findByUser(user);
+        return ResponseEntity.ok(listTransac);
+    }
+}
---
+++ src/main/java/com/paymybuddy/project/controller/TransactionController.java
@@ -0,0 +1,38 @@
+package com.paymybuddy.project.controller;
+
+import com.paymybuddy.project.dto.TransactionDTO;
+import com.paymybuddy.project.service.TransactionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Transactional(propagation = Propagation.REQUIRES_NEW)
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transacService;
+
+    Logger LOGGER = LoggerFactory.getLogger(TransactionController.class);
+
+    /**
+     * Payment between users
+     * @param transac the transaction infos
+     * @return 200 if successful | 400 if failed
+     */
+    @PostMapping(value = "/transaction")
+    public ResponseEntity makeTransaction(@RequestBody TransactionDTO transac){
+        if(transacService.makePayment(transac)){
+            LOGGER.info("Transaction success");
+            return ResponseEntity.ok(transac);
+        } else {
+            return ResponseEntity.badRequest().body(transac);
+        }
+    }
+}
